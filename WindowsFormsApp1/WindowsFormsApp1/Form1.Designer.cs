76ff928ca8c094081718e66a374e7d608a729d54
TienDuong <duonghungphuoctien24@gmail.com>
2024-03-04 09:03:08 +0700

    change2

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.label1.Size = new System.Drawing.Size(464, 51);
             this.label1.TabIndex = 0;
             this.label1.Text = "Xin chào các bạn êu \\";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
